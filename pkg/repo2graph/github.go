9ba5372932b957d81d614db38c9d577036720291
Marcos Garcia <thiamath@gmail.com>
2019-10-25 20:37:51 +0200

    First function

---
+++ pkg/repo2graph/github.go
@@ -0,0 +1,28 @@
+package repo2graph
+
+import (
+	"context"
+	"github.com/google/go-github/github"
+	"github.com/rs/zerolog/log"
+)
+
+func GetRepositories(ghClient *github.Client, ctx context.Context) ([]*github.Repository, error) {
+	options := github.RepositoryListOptions{
+		Affiliation: "organization_member",
+		ListOptions: github.ListOptions{PerPage: 25},
+	}
+	var repositories []*github.Repository
+	for {
+		pagedRepos, response, err := ghClient.Repositories.List(ctx, "", &options)
+		if err != nil {
+			log.Error().Err(err).Msg("error listing repos")
+			return nil, err
+		}
+		repositories = append(repositories, pagedRepos...)
+		if response.NextPage == 0 {
+			break
+		}
+		options.Page = response.NextPage
+	}
+	return repositories, nil
+}
