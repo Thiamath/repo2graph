2e20f3d61c00b0cec2d8557c008daae1d6b1eeae
thiamath <thiamath@gmail.com>
2019-10-28 08:57:39 +0100

    [WIP] Creating a server to serve the graphs

--- internal/server.go
+++ internal/server.go
@@ -1,8 +1,16 @@
 package internal
 
 import (
+	"context"
+	r2g "github.com/Thiamath/repo2graph/github"
+	"github.com/google/go-github/github"
 	"log"
 	"net/http"
+	"time"
+)
+
+var (
+	ctx context.Context
 )
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
@@ -10,8 +18,19 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte("This is an example server.\n"))
 }
 
+func getNodes(w http.ResponseWriter, r *http.Request) {
+	ghToken := r.URL.Query()["GITHUB_TOKEN"]
+	ghClient := github.NewClient(oauthClient)
+	getReposCtx, getReposCancel := context.WithTimeout(ctx, time.Minute)
+	repositories := r2g.GetRepositories(ghClient, ctx)
+	getReposCancel()
+	nodes := r2g.CraftNodes(repositories)
+}
+
 func StartServer() {
+	ctx = context.Background()
 	http.HandleFunc("/hello", HelloServer)
+	http.HandleFunc("/getNodes", getNodes)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
